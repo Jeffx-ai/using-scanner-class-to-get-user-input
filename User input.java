22b2f33419f7a5113775f03b23d2ab649749ba52
Geoff <85445401+Jeffx-ai@users.noreply.github.com>
2021-07-25 09:42:08 +0300

    Create User input.java

---
+++ User input.java
@@ -0,0 +1,11 @@
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Enter username and press Enter
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Username is: " + userName);        
+  }
+}
